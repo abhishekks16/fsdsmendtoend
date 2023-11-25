61d8c965859c52e313a569e9e82268405c290fe4
Abhishek K S <abhishekks1617@gmail.com>
2023-11-25 08:10:29 +0530

    added mlflow

--- src/DiamondPricePrediction/pipelines/training_pipeline.py
+++ src/DiamondPricePrediction/pipelines/training_pipeline.py
@@ -1,6 +1,7 @@
 from src.DiamondPricePrediction.components.data_ingestion import DataIngestion
 from src.DiamondPricePrediction.components.data_transformation import DataTransformation
 from src.DiamondPricePrediction.components.model_trainer import ModelTrainer
+from src.DiamondPricePrediction.components.model_evaluation import ModelEvaluation
 
 import os
 import sys
@@ -16,6 +17,10 @@ data_transformation=DataTransformation()
 
 train_arr,test_arr=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
 
-
+# To train the model
 model_trainer_obj=ModelTrainer()
 model_trainer_obj.initiate_model_training(train_arr,test_arr)
+
+# To evaluvalue the model
+model_eval_obj = ModelEvaluation()
+model_eval_obj.initiate_model_evaluation(train_arr,test_arr)
